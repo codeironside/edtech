dc76676c083dc4d56875b03a3901883fa80263b9
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2025-02-05 21:07:52 +0100

    background skeletal work

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/getoneproduct/{id}', [ProductController::class, 'show']);
+
+Route::get('/product/search/{name}', [ProductController::class, 'search']);
+
+//protected routes
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/updateproduct/{id}', [ProductController::class, 'update']);
+    Route::delete('/deleteproduct/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
